e124c2104647dfb46faa829be9656a0c0fbede87
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-09-24 11:23:42 -0600

    feat

---
+++ functions.py
@@ -0,0 +1,74 @@
+#https://www.geeksforgeeks.org/euclidean-algorithms-basic-and-extended/
+#https://www.techiedelight.com/extended-euclidean-algorithm-implementation/
+#https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/
+#Algoritmos de clase
+
+
+import numpy as np
+def gcd(a,b):
+    a = abs(a)
+    b = abs(b)
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
+
+def egcd(a, b):
+    xp  = 0
+    yp = 1
+    if a == 0:
+        return b, xp, yp
+    else:
+        gcd, x, y = egcd(b % a, a)
+        xp = y - (b // a) * x
+        yp = x
+        return gcd,xp, yp
+
+
+def modInverse(a,n):
+
+    g,x,y =egcd(a,n)
+    if(g!=1):
+        return 'No existe inverso'
+    else:
+        res = x%n
+        return res
+
+def testFermat(n,k):
+    prime = True
+    pruebas = []
+    for i in range(0,k):
+        a = np.random.randint(2,n-2)
+        pruebas.append(a)
+        if (binpow(a,n-1)%n)!=1:
+            prime = False
+    if prime:
+        return prime,pruebas
+    else:
+        return prime,a
+
+#https://cp-algorithms.com/algebra/binary-exp.html
+def binpow(a,b):
+    if b==0:
+        return 1
+    result = binpow(a,b//2)
+    if b % 2:
+        return result * result *a
+    else:
+        return result*result
+
+def generator(longitud, cantidad):
+    numb = '1'+(longitud-1)*'0'
+    numb = int(numb)
+    print(numb)
+    print(numb*100)
+    primos = []
+    while len(primos) < 5:
+        numero = np.random.randint(numb, numb*100)
+        if testFermat(numero,5)[0]==True:
+            print('Se genero un primo')
+            primos.append(numero)
+        else:
+            pass
+
+    return primos
---
+++ main.py
@@ -0,0 +1,41 @@
+from functions import gcd, egcd, modInverse, testFermat,binpow, generator
+
+#a= 1036,b= 240;•a= 22896,b= 192;•a= 689161,b= 378851.
+print('\n GDC Euclids Algorithm')
+print(gcd(1036,240))
+print(gcd(22896,192))
+print(gcd(689161,378851))
+
+#•a= 1036,b= 240;•a= 8753,b= 3354;•a= 2021,b= 43.
+print('\n Extended Euclids Algorithm')
+print(egcd(1036,240))
+print(egcd(8753,3354))
+print(egcd(2021,43))
+
+print('\n Mod Inverse')
+#•a= 47,n= 2020;•a= 31,b= 1234;•a= 65,b= 17316.
+print(modInverse(47,2020))
+print(modInverse(31,1234))
+print(modInverse(65,17316))
+
+print('\n Fermat test')
+#FALTA COMPARAR CON EL VALOR DE LA TABLA DE PRIMOS PARA COMPROBAR EFICACIA
+#1317,2709,3257,3911,4279,5497,6311,7223,8431,9203.
+print(testFermat(1317,5))
+print(testFermat(2709,5))
+print(testFermat(3257,5))
+print(testFermat(3911,5))
+print(testFermat(4279,5))
+print(testFermat(5497,5))
+print(testFermat(6311,5))
+print(testFermat(7223,5))
+print(testFermat(8431,5))
+print(testFermat(9203,5))
+
+#Generador
+print('\n Generator of primes with Fermat')
+longitud = int(input('Ingrese la longitud de los numeros primos: '))
+cantidad = int(input('Ingrese la cantidad de primos a generar: '))
+
+
+print(generator(longitud,cantidad))
